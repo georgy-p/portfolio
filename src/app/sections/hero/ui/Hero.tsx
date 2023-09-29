aee45114bde7059e964c28177a66688cafafff2f
George <81578359+georgy-p@users.noreply.github.com>
2023-09-29 15:47:55 +0300

    update text

--- src/app/sections/hero/ui/Hero.tsx
+++ src/app/sections/hero/ui/Hero.tsx
@@ -1,17 +1,21 @@
 import cn from 'classnames';
 import cls from './Hero.module.scss';
 import { Container } from 'app/shared/container';
-import photo from 'app/assets/img/hero.jpg';
+import photo from 'app/assets/imgs/hero.jpg';
 
 export const Hero = () => {
     return (
-        <Container className={cn(cls.Hero, 'grid')}>
-            <p className={cls.greeting}>
-                Привет 👋, <br /> Меня зовут Георгий Пигарь
-                <br /> Разрабатываю веб-приложения
-            </p>
-            <div className={cls.circle}>
-                <img className={cls.img} alt={'hero photo'} src={photo} />
+        <Container className={cn(cls.Hero)}>
+            <div className={cn(cls.inner_container, 'grid')}>
+                <div className={cn(cls.circle)}>
+                    <img className={cls.img} alt={'hero photo'} src={photo} />
+                </div>
+                <p className={cls.greeting}>
+                    Привет 👋, <br /> Меня зовут{' '}
+                    <span className={cls.accent_text}>Георгий Пигарь</span>
+                    <br /> Я разрабатываю функциональные <br /> и адаптивные
+                    веб-приложения
+                </p>
             </div>
         </Container>
     );
