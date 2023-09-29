37156dbf4e6fb0e8d41f235dde5c21dc398e4de7
George <81578359+georgy-p@users.noreply.github.com>
2023-09-29 15:43:55 +0300

    create technoligis list

---
+++ src/app/sections/stack/technologies/technologis.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactLogo from 'app/assets/icons/react.svg';
+import TSLogo from 'app/assets/icons/ts.svg';
+import ReduxLogo from 'app/assets/icons/redux.svg';
+import JSLogo from 'app/assets/icons/js.svg';
+import HTMLLogo from 'app/assets/icons/html.svg';
+import CSSLogo from 'app/assets/icons/css.svg';
+import SassLogo from 'app/assets/icons/sass.svg';
+import WebpackLogo from 'app/assets/icons/webpack.svg';
+import JestLogo from 'app/assets/icons/jest.svg';
+import GitLogo from 'app/assets/icons/git.svg';
+import GulpLogo from 'app/assets/icons/gulp.svg';
+import NodeJSLogo from 'app/assets/icons/nodejs.svg';
+
+export const technologies = {
+    React: <ReactLogo />,
+    TypeScript: <TSLogo />,
+    Redux: <ReduxLogo />,
+    JavaScript: <JSLogo />,
+    HTML: <HTMLLogo />,
+    CSS: <CSSLogo />,
+    Sass: <SassLogo />,
+    Webpack: <WebpackLogo />,
+    Jest: <JestLogo />,
+    Git: <GitLogo />,
+    Gulp: <GulpLogo />,
+    NodeJS: <NodeJSLogo />,
+};
