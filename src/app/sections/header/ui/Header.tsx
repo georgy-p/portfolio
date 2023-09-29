8a1dbc2113a2cc2fcd0eefdedbf4cfde9d4efc3c
George <81578359+georgy-p@users.noreply.github.com>
2023-09-29 15:50:16 +0300

    add links

--- src/app/sections/header/ui/Header.tsx
+++ src/app/sections/header/ui/Header.tsx
@@ -5,6 +5,7 @@ import Github from 'app/assets/icons/github.svg';
 import Telegram from 'app/assets/icons/telegram.svg';
 import Linkedin from 'app/assets/icons/linkedin.svg';
 import cn from 'classnames';
+import React from 'react';
 
 export const Header: React.FC = () => {
     return (
@@ -14,9 +15,28 @@ export const Header: React.FC = () => {
             >
                 <Navbar />
                 <div className={cls.icons}>
-                    <Github />
-                    <Telegram />
-                    <Linkedin />
+                    <a
+                        href='https://github.com/georgy-p'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        <Github className={cls.icon} />
+                    </a>
+                    <a
+                        href='https://t.me/pgeorgy'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        {' '}
+                        <Telegram className={cls.icon} />
+                    </a>
+                    <a
+                        href='https://www.linkedin.com/in/georgy-pigar/'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        <Linkedin className={cls.icon} />
+                    </a>
                 </div>
             </Container>
         </header>
